f3b45e6c0b3dd2875d8e75b375593d8653165d9f
thi-rodrigues <thiagorodrigues1203@outlook.com>
2020-05-29 00:30:27 -0300

    Criado o CRUD de Cargo

--- src/main/java/com/boot/web/controller/CargoController.java
+++ src/main/java/com/boot/web/controller/CargoController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -31,7 +33,8 @@ public class CargoController {
 	}
 	
 	@GetMapping("/listar")
-	public String cargos() {
+	public String cargos(ModelMap model) {
+		model.addAttribute("cargos", cargoService.buscarTodos());
 		return "/cargo/lista";
 	}
 	
@@ -41,9 +44,34 @@ public class CargoController {
 		attr.addFlashAttribute("success", "Cargo inserido com sucesso.");
 		return "redirect:/cargos/cadastrar";
 	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.buscarPorId(id));
+		return "cargo/cadastro";
+	}
 		
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.editar(cargo);
+		attr.addFlashAttribute("success", "Registro atualizado com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr){
+		if (cargoService.cargoTemFuncionarios(id)) {
+			attr.addFlashAttribute("fail", "Cargo não excluido. Tem funcionário(s) vinculado(s).");
+		} else {
+			cargoService.excluir(id);
+			attr.addFlashAttribute("success", "Cargo excluido com sucesso.");
+		}
+		return "redirect:/cargos/listar";
+	}
+	
 	@ModelAttribute("departamentos")
 	public List<Departamento> listaDeDepartamentos(){
 		return departamentoService.buscarTodos();
 	}
+	
 }
--- src/main/java/com/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/boot/web/controller/FuncionarioController.java
@@ -1,20 +1,58 @@
 package com.boot.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.boot.web.domain.Cargo;
+import com.boot.web.domain.Funcionario;
+import com.boot.web.domain.UF;
+import com.boot.web.service.CargoService;
+import com.boot.web.service.FuncionarioService;
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 
+	@Autowired
+	FuncionarioService funcionarioService;
+	
+	@Autowired
+	CargoService cargoService;
+	
 	@GetMapping("/cadastrar")
 	public String cadastrar() {
 		return "/funcionario/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("funcionarios", funcionarioService.buscarTodos());
 		return "/funcionario/lista";
 	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.salvar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário cadastrado com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+	
+	@ModelAttribute("cargos")
+	public List<Cargo> getCargos()	{
+		return cargoService.buscarTodos();
+	}
+	
+	@ModelAttribute("ufs")
+	public UF[] getUfs() {
+		return UF.values();
+	}
+	
 }
